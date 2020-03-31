e0686c34508089ae51341d483e44dca6ba41e105
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 18:41:05 +0530

    constructing setter and getter

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,37 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.student;
 // to store student information and properties
 public class Student {
+    String name;
+    int backLogCounter;
+    int appearingCounter;
+
+    public Student(String name, int backLogCounter, int appearingCounter) {
+        this.name = name;
+        this.backLogCounter = backLogCounter;
+        this.appearingCounter = appearingCounter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public int getAppearingCounter() {
+        return appearingCounter;
+    }
+
+    public void setAppearingCounter(int appearingCounter) {
+        this.appearingCounter = appearingCounter;
+    }
 }
