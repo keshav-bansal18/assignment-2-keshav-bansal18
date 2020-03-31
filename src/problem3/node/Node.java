9dd1fb837eb6f9fbf5d447e9558696633ce0cc80
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 18:27:50 +0530

    create node class and getter and setter

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,38 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.node;
 
 public class Node {
+    String name;
+    int rollNo;
+    Node next;
+
+    public Node(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.next = next;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
