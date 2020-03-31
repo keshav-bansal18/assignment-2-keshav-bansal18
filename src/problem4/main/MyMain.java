ff90740c88bc4f602041c4544abd2ad6869f91b1
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 18:38:50 +0530

    inserting values and displaying the preOrder successors

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,11 +1,21 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:08 PM
- */
 package problem4.main;
+
+import problem4.binarySearchTree.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue mq = new MyQueue();
+        MyBinarySearchTree mbst = new MyBinarySearchTree(mq);
+        mbst.insert(12);
+        mbst.insert(118);
+        mbst.insert(123);
+        mbst.insert(5);
+        mbst.insert(543);
+        mbst.preOrder(mbst.getRoot());
+        mq.show();
+
+    }
 }
