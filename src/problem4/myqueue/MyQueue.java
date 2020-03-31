3d04ddccddf1283e664d979e89ad78e80f7c3c69
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 18:37:35 +0530

    construct show method to display numers

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,30 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
- */
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.linkedlist.LinkedList;
+
 public class MyQueue {
+    LinkedList ll = new LinkedList();
+
+    public int peek(int data) {
+        if (ll.search(data) == null) {
+            return -1;
+        }
+        return ll.search(data).getData();
+    }
+
+    public void addqueue(int data) {
+        ll.addLast(data);
+
+    }
+
+    public void delqueue() {
+        ll.deleteFirst();
+    }
+
+    public void show() {
+        ll.display();
+    }
+
+
 }
