e2e103a62006331ae8fb04a4a819f3d62b64c7e5
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 18:30:25 +0530

    adding name and rollnumbers to display them in Main class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,11 +1,15 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue mpq = new MyPriorityQueue();
+        mpq.push("ashi", 1);
+        mpq.push("anshika", 2);
+        mpq.push("anamika", 3);
+        mpq.display();
+    }
 }
