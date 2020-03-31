e48691434de84274599345ba73ddfb68b152278a
keshav-bansal18 <bansal.keshav.mathura@gmail.com>
2020-03-31 18:40:16 +0530

    constructing setter and getter

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,30 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    Student stu;
+    Node next;
+
+    public Node(Student stu) {
+        this.stu = stu;
+        this.next = null;
+    }
+
+    public Student getStu() {
+        return stu;
+    }
+
+    public void setStu(Student stu) {
+        this.stu = stu;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
